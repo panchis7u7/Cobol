f2b3fef2b01dbde8422021ebe958b095c404476b
Panchis7u7 <smadrigal.rod@gmail.com>
2020-10-18 22:35:47 -0500

    Hola mundo en cobol.

---
+++ Hola mundo/HolaCobol.cob
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author: Carlos Sebastian Madrigal Rodriguez.
+      * Date: 18/10/20.
+      * Purpose: Aprendizaje.
+      * Tectonics: cobc.
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HOLA-COBOL.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hola Mundo!"
+            STOP RUN.
+       END PROGRAM HOLA-COBOL.
